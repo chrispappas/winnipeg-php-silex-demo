0d0f92b3513d27f0babc5cbff4e1516d967b7945
Chris Pappas <pappas.christopher@gmail.com>
2013-09-24 00:11:45 -0500

    Add Twig, register it with Silex, and create a very basic template and route for /

--- web/index.php
+++ web/index.php
@@ -5,6 +5,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// register the TwigServiceProvider so we can render templates using Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/../views',     // changed to reflect the /views dir being sibling to /web
+));
+
+$app->get('/', function () use ($app) {
+	return $app['twig']->render('index.html.twig');
+});
+
 $app->get('/hello/{name}', function ($name) use ($app) {
 	return 'Hello '.$app->escape($name);
 });
